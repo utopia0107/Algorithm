3d42c1b6c9c3c4af66852cdf372413322904a1e1
민수 정 <utopia0716@gmail.com>
2019-12-11 18:37:56 +0900

    초기 설정

---
+++ Algorithm/Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 민수 정  on 11/12/2019.
+//  Copyright © 2019 Minsu Jeong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
