5bbae7ccdf14cb404baeb142ec6119692651fa20
utopia0107 <20143044@vision.hoseo.edu>
2019-12-12 19:37:36 +0900

    땅따먹기

---
+++ Algorithm/Algorithm/Programmers/Level_2/GroundEating.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int dp[100000][4];
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    for(int i=0;i<4;i++)
+    {
+        dp[0][i]=land[0][i];
+    }
+    for(int i=1;i<land.size();i++)
+    {
+        dp[i][0]=land[i][0] +max(dp[i-1][1],max(dp[i-1][2],dp[i-1][3]));
+        dp[i][1]=land[i][1] +max(dp[i-1][0],max(dp[i-1][2],dp[i-1][3]));
+        dp[i][2]=land[i][2] +max(dp[i-1][0],max(dp[i-1][1],dp[i-1][3]));
+        dp[i][3]=land[i][3] +max(dp[i-1][0],max(dp[i-1][1],dp[i-1][2]));
+    }
+    int index=land.size()-1;
+    answer=max(dp[index][0],max(dp[index][1],max(dp[index][2],dp[index][3])));
+
+    return answer;
+}
